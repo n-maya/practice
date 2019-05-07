d7aca1b7b2323e218073b866817d223a24c96cbd
n-maya <maya.nakayasu@gmail.com>
2019-05-07 21:54:54 +0900

    Facebook Login Auth

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Socialite;
+use App\User;
+use Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    //facebook認証
+    //ログインボタンからリンク
+    public function socialLogin($social)
+    {
+        return Socialite::driver($social)->redirect();
+    }
+
+    //Callback処理
+    public function handleProviderCallback($social)
+    {
+        //ソーシャルサービス（情報）を取得
+        $userSocial = Socialite::driver($social)->stateless()->user();
+        //emailで登録を調べる
+        $user = User::where(['email' => $userSocial->getEmail()])->first();
+
+        //登録（email）の有無で分岐
+        if($user){
+
+            //登録あればそのままログイン（2回目以降）
+            Auth::login($user);
+            return redirect('/home');
+
+        }else{
+
+            //なければ登録（初回）
+            $newuser = new User;
+            $newuser->name = $userSocial->getName();
+            $newuser->email = $userSocial->getEmail();
+            $newuser->save();
+
+            //そのままログイン
+            Auth::login($newuser);
+            return redirect('/home');
+
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//facebook認証
+
+Route::get('/login/{social}', 'Auth\LoginController@socialLogin')->where('social', 'facebook|twitter');
+Route::get('/login/{social}/callback', 'Auth\LoginController@handleProviderCallback')->where('social', 'facebook|twitter');
